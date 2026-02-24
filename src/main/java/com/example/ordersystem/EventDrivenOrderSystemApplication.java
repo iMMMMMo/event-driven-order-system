fbbb96510a3af48fb05da556ce4c4ece7898afd1
Mateusz Kreczmer <mateusz.kreczmer7@gmail.com>
2026-02-24 23:08:44 +0100

    chore: initialize project with Spring Boot, Docker and health endpoint

---
+++ src/main/java/com/example/ordersystem/EventDrivenOrderSystemApplication.java
@@ -0,0 +1,24 @@
+package com.example.ordersystem;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenOrderSystemApplication {
+
+    public static void main(String[] args)
+    {
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
+        System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+
+        SpringApplication.run(EventDrivenOrderSystemApplication.class, args);
+    }
+
+}
